5414ff256848e6e5042e184f6c5a26351bb7612b
hi019012 <67726661+hi019012@users.noreply.github.com>
2022-02-18 11:09:43 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+import base64
+import json
+from flask import Flask, render_template, request
+from flask.wrappers import Response
+from predictor import predict_digit_in_base64
+
+app = Flask(__name__, static_folder='.', template_folder='.')
+
+@app.route('/', methods=['GET'])
+def hello():
+    print('GET / received.')
+    # Return index.html.
+    return render_template('index.html')
+
+@app.route('/predictions', methods=['POST'])
+def predict():
+    print('POST /prediction received.')
+
+    # Retrieve the image data in base64 from the request body.
+    data = request.get_json(force=True)
+    print('Request body: \n', data)
+    image_in_base64 = data['image']
+
+    # Predict the handwritten digit in the image using predict_digit_in_base64().
+    predicted_digit, prediction = predict_digit_in_base64(image_in_base64)
+
+    # Create a response in JSON.
+    response = {
+        'predictedDigit': predicted_digit,
+        'prediction': prediction
+    }
+    response_in_json_string = json.dumps(response)
+    print('Response: \n', response_in_json_string)
+
+    # Return the response.
+    return response_in_json_string
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
---
+++ predictor.py
@@ -0,0 +1,76 @@
+import io
+import base64
+import numpy as np
+from tensorflow.keras.models import load_model
+from PIL import Image
+
+# Load the trained MNIST model.
+model = load_model('./model.h5')
+
+def predict_digit(image_in_binary):
+    # Create an Image object from image_in_binary.
+    image = Image.open(io.BytesIO(image_in_binary))
+
+    # If the image size is NOT 28x28, resize to 28x28.
+    # 変更
+    print('Original size: ', image.size)
+    if image.size != (116,82):
+        image = image.resize((116,82))
+    print('Reshaped size: ',image.size)
+
+    # 変更
+    # If the image mode is NOT gray-scale, convert RGB to gray-scale.
+    print('Original mode: ', image.mode)
+    if image.mode != 'RGB' and image.mode != '1':
+        image = image.convert('RGB')
+    print('Converted mode: ', image.mode)
+
+    # Create a numpy array in the shape of (1, 28, 28).
+    # 変更
+    image_array = np.array(image).reshape(1, 116, 82, 3)
+    image_array = image_array / 255.0
+    # Predict the probability list for 10 digits using the trained MNIST model,
+    # and select the most probable digit.
+    
+    # 変更
+    predictions = model.predict(image_array)
+    predictions = predictions*100
+    if(predictions < 50):
+        predicted_digits = 0
+    else:
+        predicted_digits = 1
+
+    print('Predictions: ', predictions)
+
+    # Return the most probable digit and the probability list for 10 digits.
+    return predicted_digits, predictions[0].tolist()
+    
+    """
+    predictions = model.predict(image_array)
+    predicted_digits = np.argmax(predictions, axis=1)
+    print('Predictions: ', predictions)
+    # Return the most probable digit and the probability list for 10 digits.
+    return (int)(predicted_digits[0]), predictions[0].tolist()
+    """
+
+def predict_digit_in_base64(image_in_base64):
+    # Decode the image data in base64 to the original binary data.
+    image_in_binary = base64.b64decode(image_in_base64)
+
+    # Predict the digit using predict_digit().
+    return predict_digit(image_in_binary)
+
+if __name__ == '__main__':
+    # Predict the handwritten digit in a PNG image file.
+    with open('data/digit-2-in-100x100x3.png', 'rb') as f:
+        image_in_binary = f.read()
+        predicted_digit, prediction = predict_digit(image_in_binary)
+        print('Predicted digit: ', predicted_digit)
+        print('Prediction details: ', prediction)
+
+    # Predict the handwritten digit in a base64 text file encoded from PNG data.
+    with open('data/digit-2-in-100x100x3.txt', 'r') as f:
+        image_in_base64 = f.read()
+        predicted_digit, prediction = predict_digit_in_base64(image_in_base64)
+        print('Predicted digit: ', predicted_digit)
+        print('Prediction details: ', prediction)
